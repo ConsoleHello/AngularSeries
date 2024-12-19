56eeb7499e818ff805c554f4cdb8204c3936cf04
Naquee <naquee@redrockcapital.in>
2024-12-19 14:40:14 +0530

    Attributes Directive completed

--- anular-app1/src/app/app.component.ts
+++ anular-app1/src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { StructurealDirComponent } from './components/directive/structureal-dir/structureal-dir.component';
+// import { StructurealDirComponent } from './components/directive/structureal-dir/structureal-dir.component';
+import { AttributeDirectiveComponent } from './components/directive/attribute-directive/attribute-directive.component';
 // import { DataBindingComponent } from './components/data-binding/data-binding.component';
 // import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
 // import { EmployeeListComponent } from './components/employee-list/employee-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, StructurealDirComponent],
+  imports: [RouterOutlet,
+    //  StructurealDirComponent,
+     AttributeDirectiveComponent
+    
+    ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   // encapsulation: ViewEncapsulation.None, 
